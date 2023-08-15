5a7929065fddf7953be9805e9d2ccdd02d6eda0c
Dhruv <dhruv.h.dhagai@gmail.com>
2023-08-15 22:46:43 +0530

    Update Version 1.0.2: OpenAi Configuration added. BUG WARNING: THE KEY PROVIDED IS NOT VALID AND RETURNS STATUS 429

--- Services/Routes/routes.js
+++ Services/Routes/routes.js
@@ -1,7 +1,7 @@
 const {newRoute, routes} = require('express-quickrouter')
 const {authenticate} = require('./middleware')
 const rts = {"r":[
-        new newRoute({name:"/", method:"GET",callback: (req,res) => {
+        new newRoute({name:"/", method:"GET",callback: async(req,res) => {
             try{res.send("Routes Are Working")}catch(e){}
         }, middleware:(req,res,next) => {console.log("Routes Are Working");next();}}), 
 ]}
--- index.js
+++ index.js
@@ -5,8 +5,29 @@ const {authenticate} = require("./Services/Routes/middleware")
 const rts = require('./Services/Routes/routes');
 const path = require("path");
 const app = express();
-// app.use(express.static(path.resolve('./public')))
-
+require('dotenv').config();
+app.use(express.static(path.resolve('./public')))
+async function f()
+{const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: "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",
+  });
+  const openai = new OpenAIApi(configuration);
+  await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: "Hi",
+    temperature: 0,
+    max_tokens: 7
+  })
+  .then((response) => {
+    console.log(response.data.choices[0].text);
+    console.log({ bot: response.data.choices[0].text });
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+};
+f();
 for (let i = 0; i < rts.r.length; i++) {
     app.use(rts.r[i], routes)
 }
