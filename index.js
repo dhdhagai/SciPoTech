37adcdbcfbae4784aa0b3e8d39ec43394a883e18
Dhruv <dhruv.h.dhagai@gmail.com>
2023-08-16 22:13:03 +0530

    Removed OpenAi Configurations. Finding Other API

--- index.js
+++ index.js
@@ -7,27 +7,6 @@ const path = require("path");
 const app = express();
 require('dotenv').config();
 app.use(express.static(path.resolve('./public')))
-async function f()
-{const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-    apiKey: "",
-  });
-  const openai = new OpenAIApi(configuration);
-  await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: "Hi",
-    temperature: 0,
-    max_tokens: 7
-  })
-  .then((response) => {
-    console.log(response.data.choices[0].text);
-    console.log({ bot: response.data.choices[0].text });
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-};
-f();
 for (let i = 0; i < rts.r.length; i++) {
     app.use(rts.r[i], routes)
 }
