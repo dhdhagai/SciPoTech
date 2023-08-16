26d8c1ee8e85c9f857b58415204d5affd81803cb
Dhruv <dhruv.h.dhagai@gmail.com>
2023-08-16 23:03:31 +0530

    Added Working Electron app that is linked to the public folder's index.html and preload.js

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const {newRoute, routes} = require("express-quickrouter")
 const {authenticate} = require("./Services/Routes/middleware")
 const rts = require('./Services/Routes/routes');
 const path = require("path");
-const app = express();
-require('dotenv').config();
-app.use(express.static(path.resolve('./public')))
-for (let i = 0; i < rts.r.length; i++) {
-    app.use(rts.r[i], routes)
+const server = express();
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, './public/preload.js')
+    }
+  })
+
+  win.loadFile('./public/index.html')
 }
-app.listen(8000)
+app.whenReady().then(() => {
+  createWindow()
+});
+require('dotenv').config();
+server.use(express.static(path.resolve('./public')))
+for (let i = 0; i < rts.r.length; i++) {
+    server.use(rts.r[i], routes)
+}
+server.listen(8000, () => {console.log("server running")})
