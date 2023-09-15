1f2c51426860eeb60b5ae9e7b31621c1eb76d639
dhdhagai <dhruv.h.dhagai@gmail.com>
2023-09-15 23:21:26 +0530

    Updated to latest version 1.0.2 with removed the not required and changed a few packages

--- build.js
+++ build.js
@@ -2,13 +2,8 @@ var electronInstaller = require('electron-winstaller');
 
 
 var settings = {
-    // Specify the folder where the built app is located
     appDirectory: './build-win32-x64',
-    // Specify the existing folder where 
-    outputDirectory: './myapp-source-built-installers',
-    // The name of the Author of the app (the name of your company)
     authors: 'Dhruv Dhagai & Sai Shinkar',
-    // The name of the executable of your built
     exe: './build.exe'
 };
 
