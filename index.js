e542cfa9eb130fad7ca2d9d8ec29940c089c6f15
dhdhagai <84077442+dhdhagai@users.noreply.github.com>
2023-10-23 00:58:15 +0530

    Rewrote index.js and removed the public folder as updated electron code to make more efficient launch

--- index.js
+++ index.js
@@ -1,101 +1,34 @@
+const { app, BrowserWindow } = require('electron');
 
-const express = require("express");
-const path = require("path");
-const server = express();
-const { app, BrowserWindow } = require("electron");
-const WhichBrowser = require("which-browser");
-require("dotenv").config();
-if (handleSquirrelEvent(app)) {
-  // squirrel event handled and app will exit in 1000ms, so don't do anything else
-  return;
-}
-function handleSquirrelEvent(application) {
-  if (process.argv.length === 1) {
-      return false;
-  }
+let window = null;
 
-  const ChildProcess = require('child_process');
-  const path = require('path');
-
-  const appFolder = path.resolve(process.execPath, '..');
-  const rootAtomFolder = path.resolve(appFolder, '..');
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function(command, args) {
-      let spawnedProcess, error;
-
-      try {
-          spawnedProcess = ChildProcess.spawn(command, args, {
-              detached: true
-          });
-      } catch (error) {}
-
-      return spawnedProcess;
-  };
-
-  const spawnUpdate = function(args) {
-      return spawn(updateDotExe, args);
-  };
-
-  const squirrelEvent = process.argv[1];
-  switch (squirrelEvent) {
-      case '--squirrel-install':
-      case '--squirrel-updated':
-          // Optionally do things such as:
-          // - Add your .exe to the PATH
-          // - Write to the registry for things like file associations and
-          //   explorer context menus
-
-          // Install desktop and start menu shortcuts
-          spawnUpdate(['--createShortcut', "SciPoTech"]);
-
-          setTimeout(application.quit, 1000);
-          return true;
-
-      case '--squirrel-uninstall':
-          // Undo anything you did in the --squirrel-install and
-          // --squirrel-updated handlers
-
-          // Remove desktop and start menu shortcuts
-          spawnUpdate(['--removeShortcut', "SciPoTech"]);
-
-          setTimeout(application.quit, 1000);
-          return true;
-
-      case '--squirrel-obsolete':
-          // This is called on the outgoing version of your app before
-          // we update to the new version - it's the opposite of
-          // --squirrel-updated
-
-          application.quit();
-          return true;
-  }
-};
-function createWindow() {
-  const win = new BrowserWindow({
+app.on('ready', () => {
+  window = new BrowserWindow({
     width: 800,
     height: 600,
+    show: false,
     webPreferences: {
-      preload: path.join(__dirname, "./public/preload.js"),
-    },
+      nodeIntegration: true, // Enable node integration
+    }
   });
 
-  win.loadFile("./public/index.html");
-}
-try {
-  app.whenReady().then(() => {
-    createWindow();
-  });
-} catch (e) {
-  console.log(
-    'Running Node.js. Please Run "npx electron ."'
-  );
-}
 
-server.use(express.static(path.resolve("./public")));
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  window.loadURL("https://scipotech.netlify.app");
+
+  window.once('ready-to-show', () => {
+    window.maximize();
+    window.show();
+  });
 });
 
-server.listen(8000)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
